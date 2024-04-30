1e042ecc00141c1d957e1db0ac7e4438bf577a27
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-05-01 05:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
-//your code here
 document.addEventListener("DOMContentLoaded", function() {
     var dragged;
 
-    document.addEventListener("drag", function(event) {
-
-    }, false);
-
     document.addEventListener("dragstart", function(event) {
-
-		dragged = event.target;
+        dragged = event.target;
         event.target.style.opacity = .5;
     }, false);
 
     document.addEventListener("dragend", function(event) {
-
-		event.target.style.opacity = "";
+        event.target.style.opacity = "";
     }, false);
 
-
-	document.addEventListener("dragover", function(event) {
-
-		event.preventDefault();
-    }, false);
-
-    document.addEventListener("dragenter", function(event) {
-
-		if (event.target.className == "drag") {
-            event.target.style.background = "lightgray";
-        }
-    }, false);
-
-    document.addEventListener("dragleave", function(event) {
-
-		if (event.target.className == "drag") {
-            event.target.style.background = "";
-        }
+    document.addEventListener("dragover", function(event) {
+        event.preventDefault();
     }, false);
 
     document.addEventListener("drop", function(event) {
-
-		event.preventDefault();
-
-		if (event.target.className == "drag") {
-
-			var temp = event.target.style.backgroundImage;
+        event.preventDefault();
+        if (event.target.className == "drag") {
+            var temp = event.target.style.backgroundImage;
             event.target.style.backgroundImage = dragged.style.backgroundImage;
             dragged.style.backgroundImage = temp;
         }
