e1a29d6c325481300264f42925f26ec74634be90
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-05-01 06:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 var draggedDiv;
 
 function drag(event) {
-    // Store the id of the dragged div
     draggedDiv = event.target;
 }
 
 function allowDrop(event) {
-    // Prevent the default behavior to allow dropping
     event.preventDefault();
 }
 
 function drop(event) {
-    // Prevent the default behavior
     event.preventDefault();
 
-    // Swap the background images of the dragged div and the target div
     var temp = event.target.style.backgroundImage;
     event.target.style.backgroundImage = draggedDiv.style.backgroundImage;
     draggedDiv.style.backgroundImage = temp;
