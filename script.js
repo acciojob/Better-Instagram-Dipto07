7cd3086c7159fdf13974839e497fd72759ef659d
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-05-01 06:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-var draggedDiv;
 
-function drag(event) {
-    draggedDiv = event.target;
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+
+const images = document.querySelectorAll(".image");
+
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
 
-function allowDrop(event) {
-    event.preventDefault();
+function allowDrop(e) {
+  e.preventDefault();
 }
 
-function drop(event) {
-    event.preventDefault();
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
 
-    var temp = event.target.style.backgroundImage;
-    event.target.style.backgroundImage = draggedDiv.style.backgroundImage;
-    draggedDiv.style.backgroundImage = temp;
+  dragdrop(clone);
+
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
+
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+
+images.forEach(dragdrop);
